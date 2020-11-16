45f738a9d7244b0cdfcb1c814f6547c66ff8d6b0
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2020-11-16 23:04:45 +0100

    V11.16

--- script/script.js
+++ script/script.js
@@ -1,17 +1,60 @@
+function buildPairs() {
+  let card;
+  let usedCards = [];
+
+  for (let i = 0; i < 16; i++) {
+    do {
+      card = Math.floor(Math.random() * 16);
+    } while (usedCards.includes(allCards[card]));
+
+    usedCards.push(allCards[card]);
+  }
+
+  for (let i = 0; i < 8; i++) {
+    pairs.push([usedCards[0], usedCards[1]]);
+    usedCards.shift(0);
+    usedCards.shift(1);
+  }
+
+  labelPairs();
+}
+
+function labelPairs() {
+  for (let index = 0; index < pairs.length; index++) {
+    for (let i = 0; i < pairs[index].length; i++) {
+      pairs[index][i].innerHTML = index;
+    }
+  }
+}
+
 const allCards = document.querySelectorAll(".card");
-let allFlippedCards = 0;
+let allFlippedCards = [];
+let pairs = [];
+
+buildPairs();
 
 // flip/unflip cards
-for (let index = 0; index < allCards.length; index++) {
-  allCards[index].addEventListener("click", function (e) {
-    let c = allCards[index];
+for (let i = 0; i < allCards.length; i++) {
+  allCards[i].addEventListener("click", function (e) {
+    let c = allCards[i];
 
-    if (!c.classList.contains("card-flipped") && allFlippedCards < 2) {
+    if (!c.classList.contains("card-flipped") && allFlippedCards.length < 2) {
       c.classList.add("card-flipped");
-      allFlippedCards++;
+      allFlippedCards.push(c);
+
+      // check if pair
+      if (allFlippedCards.length == 2) {
+        if (allFlippedCards[0].innerHTML == allFlippedCards[1].innerHTML) {
+          allFlippedCards.forEach((item) => {
+            item.classList.add("card-matched");
+            item.classList.remove("card-flipped");
+          });
+        }
+        allFlippedCards = [];
+      }
     } else if (c.classList.contains("card-flipped")) {
       c.classList.remove("card-flipped");
-      allFlippedCards--;
+      allFlippedCards.pop(c);
     }
   });
 }
